b6c924f997762278a4eb12b7b641f470f9d4ee5c
刘峰 <peter.liufeng@qq.com>
2023-12-12 10:30:13 +0800

    +add std relative demo

---
+++ 19Std/hash_key.rs
@@ -0,0 +1,58 @@
+use std::collections::HashMap;
+
+// Eq 要求你对此类型推导 PartiaEq。
+#[derive(PartialEq, Eq, Hash)]
+struct Account<'a>{
+    username: &'a str,
+    password: &'a str,
+}
+
+struct AccountInfo<'a>{
+    name: &'a str,
+    email: &'a str,
+}
+
+type Accounts<'a> = HashMap<Account<'a>, AccountInfo<'a>>;
+
+fn try_logon<'a>(accounts: &Accounts<'a>,
+        username: &'a str, password: &'a str){
+    println!("Username: {}", username);
+    println!("Password: {}", password);
+    println!("Attempting logon...");
+
+    let logon = Account {
+        username: username,
+        password: password,
+    };
+
+    match accounts.get(&logon) {
+        Some(account_info) => {
+            println!("Successful logon!");
+            println!("Name: {}", account_info.name);
+            println!("Email: {}", account_info.email);
+        },
+        _ => println!("Login failed!"),
+    }
+}
+
+fn main(){
+    let mut accounts: Accounts = HashMap::new();
+
+    let account = Account {
+        username: "j.everyman",
+        password: "password123",
+    };
+
+    let account_info = AccountInfo {
+        name: "John Everyman",
+        email: "j.everyman@email.com",
+    };
+
+    accounts.insert(account, account_info);
+
+    try_logon(&accounts, "j.everyman", "psasword123");
+
+    try_logon(&accounts, "j.everyman", "password123");
+}
+//问题1：作为Key的必须有Hash函数实现，或者声明实现。它的hash算法是如何保证hash值不同？
+//问题2：hash的设计都是为了快速查找。在GPT中问C#中计算hash的结果并不完整，继续实现之。cpp的stl也有这个，如何使用。
---
+++ 20StdMisc/wait.rs
@@ -0,0 +1,9 @@
+use std::process::Command;
+
+fn main() {
+    let mut child = Command::new("sleep").arg("5").spawn().unwrap();
+    let _result = child.wait().unwrap();
+
+    println!("reached end of main");
+}
+//问题1：没有这个指令，找个替换的。
